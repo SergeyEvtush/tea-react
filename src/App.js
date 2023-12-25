ff2c6b22a1a155a8f61427806068d9d7215aef1c
SergeyEvtush <evtushru208@gmail.com>
2023-12-25 09:20:09 +0200

    start the developing from monday

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import Header from "./Components/header/Header";
 import Main from "./Components/main/Main";
 import Footer from "./Components/Footer";
 function App() {
-  const data = {
-    mainMenu: ["Black", "Green", "White", "Oolong", "Pu-erh"],
-	  mobileMenu: ["Contact Us", "Our Blog", "Shipping and Delivery"],
-	  titleText: "A cup of tea is a moment of calm in the chaos of life",
-	  paragraphTitle: "Wide selection of high-quality teas from all over the world, carefully curated by tea experts.",
-	  bloquoteText:"Tea is not just a drink, it's a way of life and a way to connect withour loved ones."
-  };
+	const data = {
+		mainMenu: ["Black", "Green", "White", "Oolong", "Pu-erh"],
+		mobileMenu: ["Contact Us", "Our Blog", "Shipping and Delivery"],
+		titleText: "A cup of tea is a moment of calm in the chaos of life",
+		paragraphTitle: "Wide selection of high-quality teas from all over the world, carefully curated by tea experts.",
+		bloquoteText: "Tea is not just a drink, it's a way of life and a way to connect withour loved ones."
+	};
 
-  return (
-    <div className="App">
-      <Header dataMenu={data}></Header>
-		  <Main dataText={[data.titleText,data.paragraphTitle,data.bloquoteText]}></Main>
-      <Footer></Footer>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header dataMenu={data}></Header>
+			<Main dataText={[data.titleText, data.paragraphTitle, data.bloquoteText]}></Main>
+			<Footer></Footer>
+		</div>
+	);
 }
 
 export default App;

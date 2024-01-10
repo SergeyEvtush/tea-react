4a2628b08b6584f1ef7db81f68e8b68677ec7e6d
SergeyEvtush <evtushru208@gmail.com>
2024-01-10 21:28:40 +0200

    реализовал открытие поста

--- src/Components/post-list/PostList.js
+++ src/Components/post-list/PostList.js
@@ -3,31 +3,21 @@ import Post from "../post/Post";
 import { Component } from "react";
 class PostList extends Component {
   constructor(props) {
-	  super(props);
-	  this.state = {
-		openPost: false
-	}
-	}
-	openPostText = () => { 
-		this.setState(({ openPost }) =>( { 
-			openPost:!openPost
-		}))
-	}
-  render() {
-	  const { dataPosts,} = this.props;
+    super(props);
+  }
 
-	  
+  render() {
+    const { dataPosts } = this.props;
+
     const postItems = dataPosts.map((el, index) => {
       return (
         <li className="blog__posts-item" key={el.postAuthor + index}>
           <Post
-					urlImage={el.postImage}
-					imageName={"blog image"}
-					blogTitle={el.postTitle}
-					postAuthor={el.postAuthor}
-					postContent={el.postContent}
-					openText={() => this.openPostText()}
-					stateText={this.state.openPost}
+            urlImage={el.postImage}
+            imageName={"blog image"}
+            blogTitle={el.postTitle}
+            postAuthor={el.postAuthor}
+            postContent={el.postContent}
           />
         </li>
       );
--- src/Components/post/Post.js
+++ src/Components/post/Post.js
@@ -3,45 +3,53 @@ import Paragraph from "../pargraph/Paragraph";
 import { Component } from "react";
 class Post extends Component {
   constructor(props) {
-	  super(props);
-	 /*  this.state = {
-		openPost: false
-	} */
-	}
-	/* openPostText = () => { 
-		this.setState(({ openPost }) =>( { 
-			openPost:!openPost
-		}))
-	} */
-	
+    super(props);
+    this.state = {
+      openPost: false,
+    };
+  }
+  openPostText = () => {
+    this.setState(({ openPost }) => ({
+      openPost: !openPost,
+    }));
+  };
+
   render() {
-	  const { urlImage,
-		  imageName,
-		  blogTitle,
-		  postAuthor,
-		  postContent,
-		  url,
-		  openText,
-		  stateText } = this.props;
-	  
-	  let postState = "post__text"
-	  stateText ? postState = "post__text active" : postState = "post__text";
+    const {
+      urlImage,
+      imageName,
+      blogTitle,
+      postAuthor,
+      postContent,
+      url,
+      openText,
+      stateText,
+    } = this.props;
+    const { openPost } = this.state;
+    let postState = "post__text";
+    openPost ? (postState = "post__text active") : (postState = "post__text");
     return (
       <div className="post">
         <img src={urlImage} alt={imageName} className="post__image" />
         <div className="post__content">
-				 <h3 className="post__title">{blogTitle}</h3>
-				 <Paragraph paragraphClass={postState } paragraphText={postContent }></Paragraph>
-          	 <Paragraph paragraphClass={"post__user"} paragraphText={postAuthor} />
-				 <div className={"btn-read"}>
-			 			<a href={url} className={"btn-read__link"} onClick={(e) => {
-				 e.preventDefault();
-				 openText();
-					 	}
-					 }>
-          				{"Read"}
-        				</a>
-      		</div>
+          <h3 className="post__title">{blogTitle}</h3>
+          <Paragraph
+            paragraphClass={postState}
+            paragraphText={postContent}
+          ></Paragraph>
+          <Paragraph paragraphClass={"post__user"} paragraphText={postAuthor} />
+          <div className={"btn-read"}>
+            <a
+              href={url}
+              className={"btn-read__link"}
+              onClick={(e) => {
+                e.preventDefault();
+                this.openPostText();
+              }}
+            >
+              {"Read"}
+            </a>
+          </div>
         </div>
       </div>
     );
